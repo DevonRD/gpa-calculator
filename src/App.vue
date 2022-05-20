c17a5f8614913b869f72953dcac70521c8ec49e1
Devon Doyle <devondoyle@outlook.com>
2022-05-20 01:33:52 -1000

    Add custom grade structure
    Doesn't work yet

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 <div id="app" :style="{background: gradient}">
-  <div id="inner-app">
+  <div id="gpa-calc-app">
     <h1>GPA Calculator 1.0</h1>
 
     <p><button class="menu-button" @click="downloadData()">Save Data</button>
@@ -15,9 +15,9 @@
               @change="updateGPA()"
               v-model="semester.included">
           <input :value="semester.title" 
-              @input="updateSemesterAttribute($event, sem_index, 'title')" 
-              placeholder="Semester name (optional)" />
-          <button @click="removeSemester(sem_index)">Delete</button>
+              @input="updateSectionAttribute($event, sem_index, 'title', 'gpa')" 
+              placeholder="Semester (e.g. Fall 2022)" />
+          <button @click="removeSection(sem_index, 'gpa')">Delete</button>
         </p>
         <div v-for="(course, course_index) in semester.courses"
             :key="course_index">
@@ -25,29 +25,74 @@
               @change="updateGPA()"
               v-model="semesters[sem_index].courses[course_index].included">
           <input :value="course.title" 
-              @input="updateCourseAttribute($event, sem_index, course_index, 'title')" 
-              placeholder="Course name (optional)" />
+              @input="updateComponentAttribute($event, sem_index, course_index, 'title')" 
+              placeholder="Course (e.g. EECS 445)" />
           <input :value="course.credits" 
-              @input="updateCourseAttribute($event, sem_index, course_index, 'credits')" 
+              @input="updateComponentAttribute($event, sem_index, course_index, 'credits')" 
               placeholder="Credit hours" />
-          <input :value="course.grade" 
-              @input="updateCourseAttribute($event, sem_index, course_index, 'grade')" 
+          <input v-if="course.custom" :value="course.grade" 
+              @input="updateComponentAttribute($event, sem_index, course_index, 'grade')" 
+              placeholder="Grade" disabled/>
+          <input v-else :value="course.grade" 
+              @input="updateComponentAttribute($event, sem_index, course_index, 'grade')" 
               placeholder="Grade" />
-          <button @click="removeCourse(sem_index, course_index)">Delete</button>
+          <button @click="calcCourse(sem_index, course_index)">Custom</button>
+          <button @click="removeComponent(sem_index, course_index, 'gpa')">Delete</button>
         </div>
-        <p><button @click="addCourse(sem_index)">Add Course</button></p>
+        <p><button @click="addComponent(sem_index, 'gpa')">Add Course</button></p>
         <p>Semester credits: {{semester.credits}}, Semester GPA: {{semester.gpa}}</p>
       </div>
     </div>
-    <p><button @click="addSemester">Add Semester</button></p>
+    <p><button @click="addSection">Add Semester</button></p>
     <p>Total credit hours: {{hours}}, Overall GPA: {{gpa}}</p>
     <p>Made on a 
       <i class="icons fa fa-solid fa-plane fa-lg"></i> 
       by <a target="_blank" href="https://devondoyle.com/">Devon Doyle</a>
     </p>
   </div>
-</div>
+  <div id="course-grade-calc">
+    <h1>Course Grade Calculator</h1>
+    <p>{{tempName}}</p>
+    <p><button class="menu-button" @click="applyCustomGrade()">Apply</button>
+    <button class="menu-button" @click="deleteCustomGrade()">Remove Custom Grade</button>
+    <button class="menu-button" @click="resetCustom()">Clear</button></p>
 
+    <div v-for="(sec, sec_index) in semesters[tempSem].courses[tempCourse].customGrades"
+        :key="sec_index" class="semester-wrapper">
+      <div class="semester-data">
+        <p>
+          <input type="checkbox" id="semester-checkbox" 
+              @change="updateCourseGrade()"
+              v-model="sec.included">
+          <input :value="sec.title" 
+              @input="updateSectionAttribute($event, sec_index, 'title', 'grade')" 
+              placeholder="Section (e.g. Quizzes)" />
+          <button @click="removeSection(sec_index, 'grade')">Delete</button>
+        </p>
+        <div v-for="(assignment, assign_index) in sec.assignments"
+            :key="assign_index">
+          <input type="checkbox" id="course-checkbox" 
+              @change="updateCourseGrade()"
+              v-model="semesters[tempSem].courses[tempCourse].customGrades[sec_index].assignments[assign_index].included">
+          <input :value="assignment.title" 
+              @input="updateComponentAttribute($event, sec_index, assign_index, 'title', 'grade')" 
+              placeholder="Assignment (e.g. Quiz 1)" />
+          <input :value="assignment.score" 
+              @input="updateComponentAttribute($event, sec_index, assign_index, 'score', 'grade')" 
+              placeholder="Score" />
+          <input :value="assignment.points" 
+              @input="updateComponentAttribute($event, sec_index, assign_index, 'points', 'grade')" 
+              placeholder="Out Of" />
+          <button @click="removeComponent(sec_index, assign_index, 'grade')">Delete</button>
+        </div>
+        <p><button @click="addComponent(sec_index, 'grade')">Add Assignment</button></p>
+        <p>Section score: {{sec.score}} / {{sec.points}}, {{sec.grade}}</p>
+      </div>
+    </div>
+    <p><button @click="addSection('grade')">Add grade section</button></p>
+    <p>Total course score: {{tempScore}} / {{tempPoints}}</p>
+  </div>
+</div>
 </template>
 
 <script>
@@ -72,19 +117,34 @@ export default {
   data() {
     return {
       //gradient: 'linear-gradient(315deg,#36fa7a,#D49CE6,#2E1BDA)',
-      gradient: 'linear-gradient(315deg,#36fa7a,#2E1BDA)',
+      gradient: 'linear-gradient(315deg,#1bd3ef,#2E1BDA)',
       mode: 'college',
       semesters: [
         {
           title: "",
-          courses: [],
+          courses: [
+            {
+              title: "",
+              credits: "",
+              grade: "",
+              color: "#ffffff",
+              included: true, 
+              custom: false,
+              customGrades: [],
+            }
+          ],
           credits: 0,
           gpa: 0,
           included: true
         }
       ],
       gpa: 0,
-      hours: 0
+      hours: 0,
+      tempSem: 0,
+      tempCourse: 0,
+      tempScore: 0,
+      tempPoints: 0,
+      tempName: ''
     }
   },
   methods: {
@@ -97,14 +157,22 @@ export default {
       this.mathtext = e.target.value;
     },
     */
-    updateSemesterAttribute(e, index, attribute) {
-      this.semesters[index][attribute] = e.target.value;
+    updateSectionAttribute(e, sec_index, attribute, mode) {
+      if (mode == 'gpa') this.semesters[sec_index][attribute] = e.target.value;
+      else this.semesters[this.tempSem].courses[this.tempCourse]
+          .customGrades[sec_index][attribute];
     },
-    updateCourseAttribute(e, sem_index, course_index, attribute) {
-      //console.log(e.target.value);
-      this.semesters[sem_index].courses[course_index][attribute] = e.target.value;
-      if (attribute != 'title') {
-        this.updateGPA();
+    updateComponentAttribute(e, sec_index, comp_index, attribute, mode='gpa') {
+      if (mode == 'gpa') {
+        this.semesters[sec_index].courses[comp_index][attribute] 
+          = e.target.value;
+        if (attribute != 'title') this.updateGPA();
+      }
+      else {
+        this.semesters[this.tempSem].courses[this.tempCourse]
+            .customGrades[sec_index].assignments[comp_index][attribute]
+          = e.target.value;
+        if (attribute != 'title') this.updateCourseGrade();
       }
     },
     updateGPA() {
@@ -129,7 +197,31 @@ export default {
       this.hours = hours;
       this.gpa = points / hours;
     },
-    addSemester() {
+    updateCourseGrade() {
+      let score = 0;
+      let points = 0;
+      this.semesters[this.tempSem].courses[this.tempCourse]
+          .customGrades.forEach(function(section) {
+        let secScore = 0;
+        let secPoints = 0;
+        section['assignments'].forEach(function(assignment) {
+          if (assignment.included) {
+            secScore += parseInt(assignment.score);
+            secPoints += parseInt(assignment.points);
+          }
+        });
+        section.score = secScore;
+        section.points = secPoints;
+        section.percent = secScore / secPoints * 100.0;
+        if (section.included) {
+          score += secScore;
+          points += secPoints;
+        }
+      });
+      this.tempScore = score;
+      this.tempPoints = points;
+    },
+    addSection(mode='gpa') {
       let newSem = {
         title: "",
         courses: [],
@@ -137,23 +229,55 @@ export default {
         gpa: 0,
         included: true
       };
-      this.semesters.push(newSem);
+      let newGradeSec = {
+        title: "",
+        assignments: [],
+        score: "",
+        points: "",
+        grade: "",
+        included: true
+      };
+      // Add a new semester to the GPA calculator
+      if (mode == 'gpa') this.semesters.push(newSem);
+      // Add a new grade section to the custom grade calculator
+      else this.semesters[this.tempSem].courses[this.tempCourse]
+          .customGrades.push(newGradeSec);
     },
-    removeSemester(sem_index) {
-      this.semesters.splice(sem_index, 1);
+    removeSection(sec_index, mode) {
+      // Remove semester from GPA calculator
+      if (mode == 'gpa') this.semesters.splice(sec_index, 1);
+      // Remove grade section from custom grade calculator
+      else this.semesters[this.tempSem].courses[this.tempCourse]
+          .customGrades.splice(sec_index, 1);
     },
-    addCourse(sem_index) {
+    addComponent(sec_index, mode) {
       let newCourse = {
         title: "",
         credits: "",
         grade: "",
         color: "#ffffff",
+        included: true, 
+        custom: false,
+        customGrades: []
+      };
+      let newAssignment = {
+        title: "",
+        score: "",
+        points: "",
         included: true
       };
-      this.semesters[sem_index].courses.push(newCourse);
+      // Add a new course in the GPA calculator
+      if (mode == 'gpa') this.semesters[sec_index].courses.push(newCourse);
+      // Add a new assignment in the course grade calculator
+      else this.semesters[this.tempSem].courses[this.tempCourse]
+          .customGrades[sec_index].assignments.push(newAssignment);
     },
-    removeCourse(sem_index, course_index) {
-      this.semesters[sem_index].courses.splice(course_index, 1);
+    removeComponent(sec_index, comp_index, mode) {
+      // Remove a course from the GPA calculator
+      if (mode == 'gpa') this.semesters[sec_index].courses.splice(comp_index, 1);
+      // Remove an assignment from the course grade calculator
+      else this.semesters[this.tempSem].courses[this.tempCourse]
+          .customGrades[sec_index].assignments.splice(comp_index, 1);
     },
     downloadData() {
       let dataString = JSON.stringify(this.semesters);
@@ -186,7 +310,17 @@ export default {
       this.semesters = [
         {
           title: "",
-          courses: [],
+          courses: [
+            {
+              title: "",
+              credits: "",
+              grade: "",
+              color: "#ffffff",
+              included: true, 
+              custom: false,
+              customGrades: []
+            }
+          ],
           credits: 0,
           gpa: 0,
           included: true
@@ -194,6 +328,37 @@ export default {
       ];
       this.gpa = 0;
       this.hours = 0;
+    },
+    calcCourse(sem_index, course_index) {
+      this.gradient = 'linear-gradient(315deg,#f49d23,#c20000)';
+      document.getElementById('gpa-calc-app').style.display = 'none';
+      document.getElementById('course-grade-calc').style.display = 'block';
+
+      this.semesters[sem_index].courses[course_index].custom = true;
+      this.tempSem = sem_index;
+      this.tempCourse = course_index;
+      this.tempName = this.semesters[sem_index].courses[course_index].title;
+    },
+    applyCustomGrade() {
+      this.semesters[this.tempSem].courses[this.tempCourse].grade = 'a-';
+      this.revertToGPA();
+    },
+    deleteCustomGrade() {
+      this.semesters[this.tempSem].courses[this.tempCourse].custom = false;
+      this.semesters[this.tempSem].courses[this.tempCourse].grade = '';
+      this.revertToGPA()
+    },
+    revertToGPA() {
+      this.tempSem = 0;
+      this.tempCourse = 0;
+      this.tempName = '';
+
+      this.gradient = 'linear-gradient(315deg,#1bd3ef,#2E1BDA)';
+      document.getElementById('gpa-calc-app').style.display = 'block';
+      document.getElementById('course-grade-calc').style.display = 'none';
+    },
+    resetCustom() {
+      this.semesters[this.tempSem].courses[this.tempCourse].customGrades = [];
     }
   }
 }
@@ -211,13 +376,20 @@ html, body {
   /*color: #2c3e50;*/
   color: white;
 }
-#inner-app {
+#gpa-calc-app {
   padding: 5%;
+  display: block;
+}
+#course-grade-calc {
+  padding: 5%;
+  display: none;
 }
 #test {
   color: #36fa7a; /* old green */
   color: #2E1BDA; /* blue */
-  color: #7e1bda;
+  color: #1bd3ef; /* light blue */
+  color: #c20000; /* crimson */
+  color: #f49d23;
 }
 button {
   margin-right: 5px;
